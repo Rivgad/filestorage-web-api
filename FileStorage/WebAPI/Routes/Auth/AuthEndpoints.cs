86151fa10918c8e427752994487ef40e2de1dfa3
Rivgad <losvermo@gmail.com>
2023-11-17 14:48:05 +0300

    add AuthEndpoints

---
+++ FileStorage/WebAPI/Routes/Auth/AuthEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+namespace WebAPI.Routes.Auth;
+
+public static class AuthEndpoints
+{
+	public static void MapAuthEndpoints(this WebApplication app)
+	{
+		var group = app.MapGroup("/").WithTags("Authentication");
+
+		group.MapPost("/login", () =>
+		{
+		});
+
+		group.MapGet("/register", () =>
+		{
+		});
+	}
+}
