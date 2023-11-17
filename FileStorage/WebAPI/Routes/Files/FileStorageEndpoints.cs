ed7c292ff1aa1f7da3fb51f55e9d37cdfc37ec32
Rivgad <losvermo@gmail.com>
2023-11-17 15:26:48 +0300

    create FileStorageEndpoints

---
+++ FileStorage/WebAPI/Routes/Files/FileStorageEndpoints.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace WebAPI.Routes.Files;
+
+public static class FileStorageEndpoints
+{
+	public static void AddFileStorageEndpointsServices(this IServiceCollection services)
+	{
+
+	}
+
+	public static void MapFileStorageEndpoints(this WebApplication app)
+	{
+		var group = app.MapGroup("/files").WithTags("Files");
+
+		group.MapPost("/upload", (IFormFileCollection files) =>
+		{
+
+		})
+		.WithSummary("Upload file or group of files");
+
+		group.MapGet("/status/{id:Guid}", (Guid id) =>
+		{
+
+		})
+	   .WithSummary("Check upload status of file or group of files");
+
+		group.MapGet("/{id:Guid}", (Guid id) =>
+		{
+
+		})
+		.WithSummary("Download file or group of files");
+
+		group.MapPost("/share/{id:Guid}", (Guid id) =>
+		{
+
+		})
+		.WithSummary("Create link for share file or group of files");
+
+		group.MapGet("/download/{code:Guid}", (Guid code) =>
+		{
+
+		})
+		.WithSummary("Download file or group of files");
+
+		group.WithOpenApi();
+	}
+}
